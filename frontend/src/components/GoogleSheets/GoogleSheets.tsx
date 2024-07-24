396cff664afcef0ed11c29426d86fe847fde5589
nami-kaze <namitagarg2003@gmail.com>
2024-07-24 18:01:35 +0530

    Add google form
    Signed-off-by: nami-kaze <namitagarg2003@gmail.com>

---
+++ frontend/src/components/GoogleSheets/GoogleSheets.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GoogleSheets = () => {
+  return (
+    <div>
+      <h2>Google Sheets</h2>
+      {/* Add your Google Sheets integration here */}
+    </div>
+  );
+};
+
+export default GoogleSheets;
